a6918658f77cdde8365f194cdc59e6c2f5473a75
Giang <phggiang@gmail.com>
2017-08-27 21:49:58 +0800

    add dummy data to h2

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO Connection(user1, user2) VALUES ('a@a.com', 'b@b.com');
+INSERT INTO Connection(user1, user2) VALUES ('c@c.com', 'b@b.com');
+INSERT INTO Connection(user1, user2) VALUES ('d@d.com', 'a@a.com');
